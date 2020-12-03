55071da504d95330317461234ddb7edbd7c51a9c
cairosanders <cairosanders@hotmail.com>
2020-12-03 11:29:36 -0800

    Add base64 encoding to rda input

--- quail/processes/wps_climdex_su.py
+++ quail/processes/wps_climdex_su.py
@@ -28,7 +28,7 @@ class ClimdexSU(Process):
                 "climdex_input",
                 "climdexInput",
                 abstract="R Object of type climdexInput",
-                supported_formats=[Format("application/rda", extension=".rda")],
+                supported_formats=[Format("application/rda", extension=".rda", encoding="base64")],
             ),
             LiteralInput(
                 "output_path",
