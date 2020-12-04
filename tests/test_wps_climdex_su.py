2c4f7c8efafb285911a8b979b8bf4bb17c5e9d96
cairosanders <cairosanders@hotmail.com>
2020-12-03 16:31:41 -0800

    Add ci_name to pytest

--- tests/test_wps_climdex_su.py
+++ tests/test_wps_climdex_su.py
@@ -7,15 +7,16 @@ from quail.processes.wps_climdex_su import ClimdexSU
 
 
 @pytest.mark.parametrize(
-    ("climdex_input"),
-    [local_path("climdexInput.rda")],
+    ("climdex_input", "ci_name"),
+    [(local_path("climdexInput.rda"), "ci")],
 )
-def test_wps_climdex_su(climdex_input):
+def test_wps_climdex_su(climdex_input, ci_name):
     with NamedTemporaryFile(
         suffix=".rda", prefix="output_", dir="/tmp", delete=True
     ) as out_file:
         datainputs = (
             f"climdex_input=@xlink:href={climdex_input};"
+            f"ci_name={ci_name};"
             f"output_path={out_file.name};"
         )
         run_wps_process(ClimdexSU(), datainputs)
