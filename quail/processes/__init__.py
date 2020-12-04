438072a5540be881f33e8f7ac1585dec795e8d9a
Sangwon Lim <sangwonl@uvic.ca>
2020-12-04 11:31:02 -0800

    minor fixes

--- quail/processes/__init__.py
+++ quail/processes/__init__.py
@@ -1,9 +1,7 @@
-from .wps_say_hello import SayHello
 from .wps_climdex_fd import ClimdexFD
 from .wps_climdex_su import ClimdexSU
 
 processes = [
-    SayHello(),
     ClimdexFD(),
     ClimdexSU(),
 ]
--- quail/processes/wps_climdex_fd.py
+++ quail/processes/wps_climdex_fd.py
@@ -134,7 +134,7 @@ class ClimdexFD(Process):
         )
 
         output_path = os.path.join(self.workdir, output_file)
-        save_rdata("frost_days", frost_days, output_path, self.workdir)
+        save_rdata(output_obj, frost_days, output_path, self.workdir)
 
         log_handler(
             self,
