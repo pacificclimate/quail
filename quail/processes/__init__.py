24e39156655813aa8ab62f7f48f3fbfd357d31b1
cairosanders <cairosanders@hotmail.com>
2020-12-04 15:26:12 -0800

    Remove su and id from processes init

--- quail/processes/__init__.py
+++ quail/processes/__init__.py
@@ -1,9 +1,5 @@
-from .wps_climdex_su import ClimdexSU
-from .wps_climdex_id import ClimdexID
 from .wps_climdex_days import ClimdexDays
 
 processes = [
     ClimdexDays(),
-    ClimdexSU(),
-    ClimdexID(),
 ]
