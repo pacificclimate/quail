13149ee553fafba9e7eab5951d6e99b0ea8160a1
cairosanders <cairosanders@hotmail.com>
2020-12-04 16:42:43 -0800

    Move test_rda_output for demos to utils

--- quail/utils.py
+++ quail/utils.py
@@ -2,7 +2,9 @@ import logging
 from rpy2 import robjects
 from rpy2.robjects.packages import isinstalled, importr
 from pywps.app.exceptions import ProcessError
-
+from urllib.request import urlretrieve
+from pkg_resources import resource_filename
+from tempfile import NamedTemporaryFile
 
 logger = logging.getLogger("PYWPS")
 logger.setLevel(logging.NOTSET)
@@ -30,3 +32,21 @@ def load_rdata_to_python(r_file, r_object_name):
 def save_python_to_rdata(r_name, py_var, r_file):
     robjects.r.assign(r_name, py_var)
     robjects.r(f"save({r_name}, file='{r_file}')")
+
+
+def test_rda_output(url, vector_name, expected_file, expected_vector_name):
+    with NamedTemporaryFile(
+        suffix=".rda", prefix="tmp_copy", dir="/tmp", delete=True
+    ) as tmp_file:
+        urlretrieve(url, tmp_file.name)
+        robjects.r(f"load(file='{tmp_file.name}')")
+
+    output_vector = robjects.r(vector_name)
+
+    robjects.r(
+        "load(file='{}')".format(resource_filename("tests",f"data/{expected_file}"))
+    )
+    expected_vector = robjects.r(expected_vector_name)
+
+    for index in range(len(expected_vector)):
+        assert str(output_vector[index]) == str(expected_vector[index])
