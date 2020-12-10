65336f2cebf4010c9317649ed04b99a9c38db93a
cairosanders <cairosanders@hotmail.com>
2020-12-10 15:49:45 -0800

    Create wps_climdex_sdi

--- quail/processes/__init__.py
+++ quail/processes/__init__.py
@@ -1,9 +1,11 @@
 from .wps_climdex_days import ClimdexDays
 from .wps_climdex_gsl import ClimdexGSL
 from .wps_climdex_mmdmt import ClimdexMMDMT
+from .wps_climdex_sdi import ClimdexSDI
 
 processes = [
     ClimdexDays(),
     ClimdexGSL(),
     ClimdexMMDMT(),
+    ClimdexSDI(),
 ]
