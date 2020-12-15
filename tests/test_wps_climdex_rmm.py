da65659ff9b120be8afaef45b6fcfcfdf3f2714c
cairosanders <cairosanders@hotmail.com>
2020-12-14 17:04:01 -0800

    Create wps_climdex_rmm and tests

---
+++ tests/test_wps_climdex_rmm.py
@@ -0,0 +1,26 @@
+import pytest
+from tempfile import NamedTemporaryFile
+
+from wps_tools.testing import run_wps_process, local_path
+from quail.processes.wps_climdex_rmm import ClimdexRMM
+
+
+@pytest.mark.parametrize(
+    ("climdex_input", "ci_name", "threshold"),
+    [
+        (local_path("climdexInput.rda"), "ci", 10.0),
+        (local_path("climdexInput.rda"), "ci", 20.0),
+        (local_path("climdexInput.rda"), "ci", 15.5),
+    ],
+)
+def test_wps_climdex_rmm(climdex_input, ci_name, threshold):
+    with NamedTemporaryFile(
+        suffix=".rda", prefix="output_", dir="/tmp", delete=True
+    ) as out_file:
+        datainputs = (
+            f"climdex_input=@xlink:href={climdex_input};"
+            f"ci_name={ci_name};"
+            f"threshold={threshold};"
+            f"output_file={out_file.name};"
+        )
+        run_wps_process(ClimdexRMM(), datainputs)
