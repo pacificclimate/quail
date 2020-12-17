33e44ce3ff203979dc2a16a36e7acf64a8f682e1
Sangwon Lim <sangwonl@uvic.ca>
2020-12-17 10:54:20 -0800

    update __init__.py

--- quail/processes/__init__.py
+++ quail/processes/__init__.py
@@ -8,6 +8,7 @@ from .wps_climdex_spells import ClimdexSpells
 from .wps_climdex_temp_pctl import ClimdexTempPctl
 from .wps_climdex_dtr import ClimdexDTR
 from .wps_climdex_ptot import ClimdexPtot
+from .wps_climdex_quantile import ClimdexQuantile
 
 processes = [
     ClimdexDays(),
@@ -20,4 +21,5 @@ processes = [
     ClimdexTempPctl(),
     ClimdexDTR(),
     ClimdexPtot(),
+    ClimdexQuantile(),
 ]
