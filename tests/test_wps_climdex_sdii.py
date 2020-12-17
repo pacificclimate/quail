acc82818650c0c224afa2b9f75c87ca5ff3d4d9d
cairosanders <cairosanders@hotmail.com>
2020-12-16 16:25:17 -0800

    Create pytest for wps_climdex_sdii

--- tests/test_wps_climdex_sdii.py
+++ tests/test_wps_climdex_sdii.py
@@ -2,24 +2,22 @@ import pytest
 from tempfile import NamedTemporaryFile
 
 from wps_tools.testing import run_wps_process, local_path
-from quail.processes.wps_climdex_dtr import ClimdexDTR
+from quail.processes.wps_climdex_sdii import ClimdexSDII
 
 
 @pytest.mark.parametrize(
-    ("climdex_input", "ci_name", "freq"),
+    ("climdex_input", "ci_name"),
     [
-        (local_path("climdexInput.rda"), "ci", "monthly"),
-        (local_path("climdexInput.rda"), "ci", "annual"),
+        (local_path("climdexInput.rda"), "ci"),
     ],
 )
-def test_wps_climdex_dtr(climdex_input, ci_name, freq):
+def test_wps_climdex_sdii(climdex_input, ci_name):
     with NamedTemporaryFile(
         suffix=".rda", prefix="output_", dir="/tmp", delete=True
     ) as out_file:
         datainputs = (
             f"climdex_input=@xlink:href={climdex_input};"
             f"ci_name={ci_name};"
-            f"freq={freq};"
             f"output_file={out_file.name};"
         )
-        run_wps_process(ClimdexDTR(), datainputs)
+        run_wps_process(ClimdexSDII(), datainputs)
