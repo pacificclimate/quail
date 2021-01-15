5fbd3a893f957a643d7c37925d4f8909eab5b0aa
cairosanders <cairosanders@hotmail.com>
2021-01-14 16:35:11 -0800

    Add test for load_rda err in utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,7 @@ from pywps.app.exceptions import ProcessError
 from tempfile import NamedTemporaryFile
 from contextlib import redirect_stderr
 
-from quail.utils import load_ci
+from quail.utils import load_ci, load_rda
 from wps_tools.testing import local_path
 
 
@@ -30,3 +30,15 @@ def test_load_ci_name_err(file_, obj_name):
     with pytest.raises(ProcessError) as e:
         load_ci(file_, obj_name)
         assert str(vars(e)["_excinfo"][1]) == "Either your file is not a valid Rdata file or the climdexInput object name is not found in this rda file"
+
+
+@pytest.mark.parametrize(
+    ("file_", "obj_name"),
+    [
+        (local_path("expected_days_data.rda"), "autumn_days"),
+    ],
+)
+def test_load_rda_err(file_, obj_name):
+    with pytest.raises(ProcessError) as e:
+        load_rda(file_, obj_name)
+        assert str(vars(e)["_excinfo"][1]) == "Either your file is not a valid Rdata file or there is no object of that name is not found in this rda file"
