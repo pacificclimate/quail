f5a1e86e93f28238ccf2a13bf31841f5a0b7d820
nikola-rados <nrados@uvic.ca>
2021-05-13 11:04:36 -0700

    Update gsl process for new io

--- quail/processes/wps_climdex_gsl.py
+++ quail/processes/wps_climdex_gsl.py
@@ -7,10 +7,10 @@ from rpy2.rinterface_lib.embedded import RRuntimeError
 
 
 from wps_tools.logging import log_handler, common_status_percentages
-from wps_tools.io import log_level, rda_output
+from wps_tools.io import rda_output
 from wps_tools.R import get_package
-from quail.utils import logger, load_cis, collect_literal_inputs
-from quail.io import climdex_input, output_file
+from quail.utils import logger, load_cis, process_inputs
+from quail.io import gsl_inputs
 
 
 class ClimdexGSL(Process):
@@ -32,22 +32,8 @@ class ClimdexGSL(Process):
                 "save_rdata": 90,
             },
         )
-        inputs = [
-            climdex_input,
-            output_file,
-            LiteralInput(
-                "gsl_mode",
-                "GSL mode",
-                abstract="Growing season length method to use. The three alternate modes provided ('GSL_first', 'GSL_max', and 'GSL_sum') are for testing purposes only.",
-                default="GSL",
-                min_occurs=0,
-                max_occurs=1,
-                allowed_values=["GSL", "GSL_first", "GSL_max", "GSL_sum"],
-                data_type="string",
-            ),
-            log_level,
-        ]
 
+        inputs = gsl_inputs
         outputs = [rda_output]
 
         super(ClimdexGSL, self).__init__(
@@ -69,8 +55,7 @@ class ClimdexGSL(Process):
         )
 
     def _handler(self, request, response):
-        output_file, gsl_mode, loglevel = collect_literal_inputs(request)
-        climdex_input = request.inputs["climdex_input"]
+        climdex_input, gsl_mode, loglevel, output_file = process_inputs(request.inputs, gsl_inputs, self.workdir)
 
         log_handler(
             self,
@@ -83,21 +68,24 @@ class ClimdexGSL(Process):
         climdex = get_package("climdex.pcic")
         vectors = []
 
-        for i in range(len(climdex_input)):
+        counter = 1
+        total = len(climdex_input)
+
+        for input in climdex_input:
             log_handler(
                 self,
                 response,
-                f"Loading climdexInputs from R data file {i}",
+                f"Loading climdexInputs from R data file {counter}/{total}",
                 logger,
                 log_level=loglevel,
                 process_step="load_rdata",
             )
-            cis = load_cis(climdex_input[i].file)
+            cis = load_cis(input)
 
             log_handler(
                 self,
                 response,
-                "Processing growing seasonal length for file {i}",
+                f"Processing growing seasonal length for file {counter}/{total}",
                 logger,
                 log_level=loglevel,
                 process_step="process",
@@ -110,9 +98,10 @@ class ClimdexGSL(Process):
                 except RRuntimeError as e:
                     raise ProcessError(msg=f"{type(e).__name__}: {str(e)}")
 
-                vector_name = f"gsl{i}_{ci_name}"
+                vector_name = f"gsl{counter}_{ci_name}"
                 robjects.r.assign(vector_name, gsl)
                 vectors.append(vector_name)
+            counter += 1
 
         log_handler(
             self,
